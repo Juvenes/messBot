5f2475835674aec752265fddc21feb48c53e3211
Juvenes <ecoleroman@gmail.com>
2022-01-28 16:20:41 +0100

    dada

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 'use strict';
-var Person = function (_name,_id) {
-  this.name = _name;
-  this.id = _id;
+
+const res = require('express/lib/response');
+const { cp } = require('fs/promises');
+
+const Person = class {
+  constructor(_name, _id) {
+    this.name = _name;
+    this.id = _id;
+    this.choices = [];
+  }
+  
+  isEmpty() {
+    return this.choices.length === 0;
+  }
+  addChoice(one) {
+    this.choices.push(one);
+  }
+  resetChoice() {
+    this.choices = [];
+  }
+  isFullChoice() {
+    return this.choices.length ===3;
+  }
 };
+
 const copain = [new Person("Roman","4576184389177701"),new Person("Test","123456789")];
 
-
-
 function getname(id) {
-  let mmas = "ERROR";
-  copain.forEach(el  =>  id===el.id ? mmas = el.name :console.log("User not found"));
-  return mmas;
+  return copain.find(o => o.id === id);
 };
 
 const
@@ -20,7 +37,14 @@ const
   app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 1337, () => {
+  console.log('webhook is listening');
+  let be = getname("123456789")
+  be.addChoice("TRIX");
+  be.addChoice("BULLRUN");
+  be.addChoice("BTCUSDT");
+  
+  call_python("123456789");});
 
 
 // Creates the endpoint for our webhook 
@@ -38,7 +62,7 @@ app.post('/webhook', (req, res) => {
         // will only ever contain one message, so we get index 0
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
-        if(getname(webhook_event.sender.id) !== "ERROR"){
+        if(getname(webhook_event.sender.id) !== undefined){
           if (webhook_event.message) {
             handleMessage(webhook_event.sender.id, webhook_event.message);
           }
@@ -92,17 +116,16 @@ console.log('NEW');
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
   let response;
-  let name_res = getname(sender_psid);
+  let person = getname(sender_psid);
   // Check if the message contains text
   if ((received_message.text ==="Salut" || received_message.text ==="yo" ||received_message.text ==="salut" || received_message.text ==="reset")) {    
-    
     // Create the payload for a basic text message
     response = {
         "attachment":{
           "type":"template",
           "payload":{
             "template_type":"button",
-            "text":`Salut ${name_res},que veux-tu faire ?`,
+            "text":`Salut ${person.name},que veux-tu faire ?`,
             "buttons":[
               {
                 "type": "postback",
@@ -135,78 +158,130 @@ function handlePostback(sender_psid, received_postback) {
   let payload = received_postback.payload;
   // Set the response based on the postback payload
   if (payload === 'SOLD_CHECK') {
-    handleSolde(sender_psid);
+    handleSolde(sender_psid);ù
   } else if (payload === 'MY_HISTORY') {
     handleHistorique(sender_psid);
   } else if (payload === 'SIMUL') {
     handleSimul(sender_psid);
+  }else if (payload === 'BULlRUN' || payload === 'CRASHRUN' || payload === 'NORMAL'){
+    handlePeriod(sender_psid,payload);
+  }else if (payload === 'TRIX'){
+    handleTrix(sender_psid,payload);
+  }else if (payload === 'BTCUSDT'|| payload === 'AVAXUSDT'||payload === 'CHZUSDT'||payload === 'LINKUSDT'||payload === 'FTMUSDT'||payload === 'MANAUSDT'||payload === 'XPRUSDT'){
+    handleCoins(sender_psid,payload);
   }
 }
-
-function handleHistorique(sender_psid){
+function handleTrix(sender_psid,payload){
   let response;
-  let name_res = getname(sender_psid);
+  let bro =getname(sender_psid);
+  bro.addChoice(payload)
   response = {
-    "recipient":{
-      "id": `${sender_psid}`
-    },
-    "message":{
-    }
+    "text": "Sur Quel type de perdiods veut-tu tester la simulation ?",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Bullrun",
+        "payload":"BULlRUN",
+        "image_url":"https://previews.123rf.com/images/asmati/asmati1602/asmati160203127/52184892-growing-signe-de-fl%C3%A8che-flat-ic%C3%B4ne-de-style-sur-fond-transparent.jpg"
+      },{
+        "content_type":"text",
+        "title":"Crashrun",
+        "payload":"CRASHRUN",
+        "image_url":"https://previews.123rf.com/images/asmati/asmati1602/asmati160203126/52184888-la-baisse-de-signe-de-fl%C3%A8che-flat-ic%C3%B4ne-de-style-sur-fond-transparent.jpg"
+      },{
+        "content_type":"text",
+        "title":"Normal(=2ans)",
+        "payload":"NORMAL",
+        "image_url":"http://www.publicdomainpictures.net/pictures/40000/velka/question-mark.jpg"
+      }
+    ]
   }
-  callSendAPI(sender_psid, response);
+ 
 }
 
-
-function handleSolde(sender_psid){
-  let response;
-  let name_res = getname(sender_psid);
-
-  callSendAPI(sender_psid, response);
-}
 
 function handleSimul(sender_psid){
   let response;
 
   response = {
-    "text": "Quel coins veux-tu simuler ?",
+    "text": "Quel Stategie veux-tu essayer?",
     "quick_replies":[
       {
         "content_type":"text",
-        "title":"BTC/USD",
-        "payload":"BTC",
-        "image_url":"https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Bitcoin-BTC-icon.png"
-      },{
-        "content_type":"text",
-        "title":"AVAX/USD",
-        "payload":"AVAX",
-        "image_url":"https://uxwing.com/wp-content/themes/uxwing/download/10-brands-and-social-media/avalanche-avax.png"
-      },{
-        "content_type":"text",
-        "title":"CHZ/USD",
-        "payload":"CHZ",
-        "image_url":"https://s2.coinmarketcap.com/static/img/coins/200x200/4066.png"
-      },{
-        "content_type":"text",
-        "title":"LINK/USD",
-        "payload":"LINK",
-        "image_url":"https://cdn-icons-png.flaticon.com/512/189/189688.png"
-      },{
-        "content_type":"text",
-        "title":"FTM/USD",
-        "payload":"FTM",
-        "image_url":"https://icoholder.com/files/img/14d6c55d8f4100303ab82e62baf21b83.png"
-      },{
-        "content_type":"text",
-        "title":"MANA/USD",
-        "payload":"MANA",
-        "image_url":"https://s2.coinmarketcap.com/static/img/coins/200x200/1966.png"
+        "title":"TRIX ",
+        "payload":"TRIX",
+        "image_url":"https://iconape.com/wp-content/png_logo_vector/trix-logo.png"
       }
     ]
   }
   callSendAPI(sender_psid, response);
 }
 
+function handlePeriod(sender_psid,payload){
+  let response;
+  let bro =getname(sender_psid);
+  bro.addChoice(payload)
+  response = {
+    "text": "Quel coins veux-tu simuler ?",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"BTC/USD",
+        "payload":"BTCUSDT",
+        "image_url":"https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Bitcoin-BTC-icon.png"
+      },{
+        "content_type":"text",
+        "title":"AVAX/USD",
+        "payload":"AVAXUSDT",
+        "image_url":"https://usethebitcoin.com/wp-content/uploads/2021/11/Avalanche-Crypto-Logo.jpeg"
+      },{
+        "content_type":"text",
+        "title":"CHZ/USD",
+        "payload":"CHZUSDT",
+        "image_url":"https://s2.coinmarketcap.com/static/img/coins/200x200/4066.png"
+      },{
+        "content_type":"text",
+        "title":"LINK/USD",
+        "payload":"LINKUSDT",
+        "image_url":"https://cdn-icons-png.flaticon.com/512/189/189688.png"
+      },{
+        "content_type":"text",
+        "title":"FTM/USD",
+        "payload":"FTMUSDT",
+        "image_url":"https://icoholder.com/files/img/14d6c55d8f4100303ab82e62baf21b83.png"
+      },{
+        "content_type":"text",
+        "title":"MANA/USD",
+        "payload":"MANAUSDT",
+        "image_url":"https://s2.coinmarketcap.com/static/img/coins/200x200/1966.png"
+      },{
+        "content_type":"text",
+        "title":"XPR/USD",
+        "payload":"XPRUSDT",
+        "image_url":"https://www.shareicon.net/data/2016/07/08/117526_ripple_512x512.png"
+      }
+    ]
+  }
+  callSendAPI(sender_psid, response);
 
+}
+function handleCoins(sender_psid,payload){
+  let response;
+  let bro =getname(sender_psid);
+  bro.addChoice(payload)
+  if( bro.isFullChoice){
+    response = {"text": "Simulation en cours ça arrive en légende"};
+    callSendAPI(sender_psid, response);
+    call_python(sender_psid);
+  }
+  else{
+    response = {"text": "Zebi y'a un probleme,renvoye:salut"};
+    callSendAPI(sender_psid, response);
+    bro.clearChoice();
+  }
+}
+
+//CALL FOR ALL 
 function callSendAPI(sender_psid, response) {
   // Construct the message body 
 
@@ -232,4 +307,49 @@ function callSendAPI(sender_psid, response) {
     console.log(res.body);
   }); 
 
+}
+
+
+/// PART SOLD
+function handleSolde(sender_psid){
+  let response;
+  callSendAPI(sender_psid, response);
+}
+function handleHistorique(sender_psid){
+  let response;
+  response = {
+    "recipient":{
+      "id": `${sender_psid}`
+    },
+    "message":{
+    }
+  }
+  callSendAPI(sender_psid, response);
+}
+
+async function handleCalculation(sender_psid){
+
+}
+function call_python (sender_psid) {
+  let bro = getname(sender_psid);
+  let response;
+  const promise = new Promise((resolve,reject)=>{
+    const {spawn}= require("child_process");
+    console.log("Lancement python")
+    const pypro =spawn('python', ["vavav.py",bro.choices[2],bro.choices[1],bro.choices[0]]);
+    pypro.stdout.on('data', (data)=> {
+      resolve(data.toString());
+  })
+  pypro.stderr.on('data', (data)=> {
+    reject(data.toString());
+  })
+  }).then(value=> {
+    response = {"text": value};
+    bro.resetChoice();
+    callSendAPI(sender_psid, response);
+    
+  }).catch(err=> {
+    console.log(err);
+  })
+  
 }
---
+++ vavav.py
@@ -0,0 +1,248 @@
+import pandas as pd
+from binance.client import Client
+import ta
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+ftxClassicPair = [
+        "BTC/USD",
+        "ETH/USD",
+        "LINK/USD",
+        "TRX/USD",
+        "BCH/USD",
+        "FTM/USD",
+        "MANA/USD",
+        "CHZ/USD",
+        "AVAX/USD"]
+
+bullrun = ["27 june 2021", "20 november 2021"]
+crashrun = ["20 october 2021", "26 january 2022"]
+longrun = ["01 january 2021", "26 january 2022"]
+
+client = Client()
+def getInfos(pair,start,end):
+    # -- Ameliorable en memorisant les bougies ! --
+    pairName = pair
+    startDate = start
+    EndDate = end
+    timeInterval = Client.KLINE_INTERVAL_1HOUR
+
+    # -- Load all price data from binance API --
+    klinesT = client.get_historical_klines(pairName, timeInterval,startDate,EndDate)
+
+    # -- Define your dataset --
+    df = pd.DataFrame(klinesT, columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore' ])
+    df['close'] = pd.to_numeric(df['close'])
+    df['high'] = pd.to_numeric(df['high'])
+    df['low'] = pd.to_numeric(df['low'])
+    df['open'] = pd.to_numeric(df['open'])
+
+    # -- Set the date to index --
+    df = df.set_index(df['timestamp'])
+    df.index = pd.to_datetime(df.index, unit='ms')
+    del df['timestamp']
+    df.drop(df.columns.difference(['open','high','low','close','volume']), 1, inplace=True)
+    return df
+def trixAddIndicate(df):
+    df['EMA200'] = ta.trend.ema_indicator(close=df['close'], window=200)
+    trixLength = 9
+    trixSignal = 21
+    df['TRIX'] = ta.trend.ema_indicator(ta.trend.ema_indicator(ta.trend.ema_indicator(close=df['close'], window=trixLength), window=trixLength), window=trixLength)
+    df['TRIX_PCT'] = df["TRIX"].pct_change()*100
+    df['TRIX_SIGNAL'] = ta.trend.sma_indicator(df['TRIX_PCT'],trixSignal)
+    df['TRIX_HISTO'] = df['TRIX_PCT'] - df['TRIX_SIGNAL']
+    df['STOCH_RSI'] = ta.momentum.stochrsi(close=df['close'], window=14, smooth1=3, smooth2=3)
+    return df
+
+
+
+
+class BackTrick:
+    def __init__(self):
+        self.usdt = 1000
+        self.makerFee = 0.0002
+        self.takerFee = 0.0007
+        # -- Do not touch these values --
+        self.initalWallet = self.usdt
+        self.wallet = self.usdt
+        self.coin = 0
+        self.lastAth = 0
+        self.stopLoss = 0
+        self.takeProfit = 500000
+        self.buyReady = True
+        self.sellReady = True
+        self.dt = pd.DataFrame(columns = ['date','position', 'reason', 'price', 'frais' ,'fiat', 'coins', 'wallet', 'drawBack'])
+     # -- Condition to BUY market --
+    def buyCondition(self ,row):
+        if row['TRIX_HISTO'] > 0 and row['STOCH_RSI'] < 0.8:
+            return True
+        else:
+            return False
+
+    # -- Condition to SELL market --  
+    def sellCondition(self ,row):
+        if row['TRIX_HISTO'] < 0 and row['STOCH_RSI'] > 0.2:
+            return True
+        else:
+            return False
+    def runBackTrix(self,df,pair):
+        dfTest = df.copy()
+        for index, row in dfTest.iterrows():
+            # -- Buy market order --
+            if self.buyCondition(row) and self.usdt > 0 and self.buyReady == True:
+                buyPrice = row['close']
+                self.coin = self.usdt / buyPrice
+                fee = self.takerFee * self.coin
+                self.coin = self.coin - fee
+                self.usdt = 0
+                self.wallet = self.coin * row['close']
+                if self.wallet > self.lastAth:
+                    self.lastAth = self.wallet
+                myrow = {'date': index, 'position': "Buy", 'reason':'Buy Market Order','price': buyPrice,'frais': fee * row['close'],'fiat': self.usdt,'coins': self.coin,'wallet': self.wallet,'drawBack':(self.wallet-self.lastAth)/self.lastAth}
+                self.dt = self.dt.append(myrow,ignore_index=True)
+            elif row['low'] < self.stopLoss and self.coin > 0:
+                sellPrice = self.stopLoss
+                self.usdt = self.coin * sellPrice
+                fee = self.makerFee * self.usdt
+                self.usdt = self.usdt - fee
+                self.coin = 0
+                self.buyReady = False
+                self.wallet = self.usdt
+
+                # -- Check if your wallet hit a new ATH to know the drawBack --
+                if self.wallet > self.lastAth:
+                    self.lastAth = self.wallet
+                
+                # -- You can uncomment the line below if you want to see logs --
+                # a += "Sell COIN at Stop Loss",sellPrice,'$ the', index)
+
+                # -- Add the trade to DT to analyse it later --
+                myrow = {'date': index,'position': "Sell", 'reason':'Sell Stop Loss','price': sellPrice,'frais': fee,'fiat': self.usdt,'coins': self.coin,'wallet': self.wallet,'drawBack':(self.wallet-self.lastAth)/self.lastAth}
+                self.dt = self.dt.append(myrow,ignore_index=True)    
+
+            # -- Sell Market Order --
+            elif self.sellCondition(row) and self.coin > 0 and self.sellReady == True:
+
+                # -- You can define here at what price you buy --
+                sellPrice = row['close']
+                self.usdt = self.coin * sellPrice
+                fee = self.takerFee * self.usdt
+                self.usdt = self.usdt - fee
+                self.coin = 0
+                self.buyReady = True
+                self.wallet = self.usdt
+
+                # -- Check if your wallet hit a new ATH to know the drawBack --
+                if self.wallet > self.lastAth:
+                    self.lastAth = self.wallet
+
+                # -- You can uncomment the line below if you want to see logs --  
+                # a += "Sell COIN at",sellPrice,'$ the', index)
+
+                # -- Add the trade to DT to analyse it later --
+                myrow = {'date': index,'position': "Sell", 'reason':'Sell Market Order','price': sellPrice,'frais': fee,'fiat': self.usdt,'coins': self.coin,'wallet': self.wallet,'drawBack':(self.wallet-self.lastAth)/self.lastAth}
+                self.dt = self.dt.append(myrow,ignore_index=True)
+            
+        # -- BackTest Analyses --
+        self.dt = self.dt.set_index(self.dt['date'])
+        self.dt.index = pd.to_datetime(self.dt.index)
+        self.dt['resultat'] = self.dt['wallet'].diff()
+        self.dt['resultat%'] = self.dt['wallet'].pct_change()*100
+        self.dt.loc[self.dt['position']=='Buy','resultat'] = None
+        self.dt.loc[self.dt['position']=='Buy','resultat%'] = None
+
+        self.dt['tradeIs'] = ''
+        self.dt.loc[self.dt['resultat']>0,'tradeIs'] = 'Good'
+        self.dt.loc[self.dt['resultat']<=0,'tradeIs'] = 'Bad'
+        iniClose = dfTest.iloc[0]['close']
+        lastClose = dfTest.iloc[len(dfTest)-1]['close']
+        holdPercentage = ((lastClose - iniClose)/iniClose) * 100
+        algoPercentage = ((self.wallet - self.initalWallet)/self.initalWallet) * 100
+        vsHoldPercentage = ((algoPercentage - holdPercentage)/holdPercentage) * 100
+
+        try:
+            tradesPerformance = round(self.dt.loc[(self.dt['tradeIs'] == 'Good') | (self.dt['tradeIs'] == 'Bad'), 'resultat%'].sum()
+                    / self.dt.loc[(self.dt['tradeIs'] == 'Good') | (self.dt['tradeIs'] == 'Bad'), 'resultat%'].count(), 2)
+        except:
+            tradesPerformance = 0
+
+        try:
+            totalGoodTrades = self.dt.groupby('tradeIs')['date'].nunique()['Good']
+            AveragePercentagePositivTrades = round(self.dt.loc[self.dt['tradeIs'] == 'Good', 'resultat%'].sum()
+                                                / self.dt.loc[self.dt['tradeIs'] == 'Good', 'resultat%'].count(), 2)
+            idbest = self.dt.loc[self.dt['tradeIs'] == 'Good', 'resultat%'].idxmax()
+            bestTrade = str(
+                round(self.dt.loc[self.dt['tradeIs'] == 'Good', 'resultat%'].max(), 2))
+        except:
+            totalGoodTrades = 0
+            AveragePercentagePositivTrades = 0
+            idbest = ''
+            bestTrade = 0
+
+        try:
+            totalBadTrades = self.dt.groupby('tradeIs')['date'].nunique()['Bad']
+            AveragePercentageNegativTrades = round(self.dt.loc[self.dt['tradeIs'] == 'Bad', 'resultat%'].sum()
+                                                / self.dt.loc[self.dt['tradeIs'] == 'Bad', 'resultat%'].count(), 2)
+            idworst = self.dt.loc[self.dt['tradeIs'] == 'Bad', 'resultat%'].idxmin()
+            worstTrade = round(self.dt.loc[self.dt['tradeIs'] == 'Bad', 'resultat%'].min(), 2)
+        except:
+            totalBadTrades = 0
+            AveragePercentageNegativTrades = 0
+            idworst = ''
+            worstTrade = 0
+
+        totalTrades = totalBadTrades + totalGoodTrades
+        winRateRatio = (totalGoodTrades/totalTrades) * 100
+        reasons = self.dt['reason'].unique()
+        a = ""
+        a += "\nPair Symbol :" +pair
+        a += "\nPeriod : [" + str(dfTest.index[0]) + "] -> [" + str(dfTest.index[len(dfTest)-1]) + "]"
+        a += "\nStarting balance :" + str(self.initalWallet) + "$"
+
+        a += "\n\n----- General Informations -----"
+        a += "\nFinal balance :"+ str(round(self.wallet, 2))+ "$"
+        a += "\nPerformance vs US Dollar :"+ str(round(algoPercentage, 2))+ "%"
+        a += "\nBuy and Hold Performence :"+ str(round(holdPercentage, 2))+ "%"
+        a += "\nPerformance vs Buy and Hold :"+ str(round(vsHoldPercentage, 2))+ "%"
+        a += "\nBest trade : +"+str(bestTrade)+ "%, the"+ str(idbest)
+        a += "\nWorst trade :"+str( worstTrade)+ "%, the"+ str(idworst)
+        a += "\nWorst drawBack :"+ str(100*round(self.dt['drawBack'].min(), 2))+ "%"
+        a += "\nTotal fees : "+ str(round(self.dt['frais'].sum(), 2))+ "$"
+        a += "\n\n----- Trades Informations -----"
+        a += "\nTotal trades on period :"+str(totalTrades)
+        a += "\nNumber of positive trades :"+ str(totalGoodTrades)
+        a += "\nNumber of negative trades : "+ str(totalBadTrades)
+        a += "\nTrades win rate ratio :"+ str(round(winRateRatio, 2))+ '%'
+        a += "\nAverage trades performance :"+str(tradesPerformance)+"%"
+        a += "\nAverage positive trades :"+ str(AveragePercentagePositivTrades)+ "%"
+        a += "\nAverage negative trades :"+ str(AveragePercentageNegativTrades)+"%"
+
+        a += "\n\n----- Trades Reasons -----"
+        reasons = self.dt['reason'].unique()
+        for r in reasons:
+            a += "\n"+str(r)+" number :"+ str(self.dt.groupby('reason')['date'].nunique()[r])
+        print(a)
+
+def main():
+    StartDate = "01 january 2021"
+    EndDate = "26 january 2022"
+    coin = sys.argv[1]
+    if (sys.argv[2] =="BULLRUN"):
+        StartDate = "27 june 2021"
+        EndDate = "20 november 2021"
+    if (sys.argv[2]=="CRASHRUN"):
+        StartDate = "20 october 2021"
+        EndDate = "26 january 2022"
+    if(sys.argv[3]== "TRIX"):
+        dt = getInfos(coin,StartDate, EndDate)
+        df = trixAddIndicate(dt)
+        back_trix=  BackTrick()
+        back_trix.runBackTrix(df,coin)
+
+if __name__ == '__main__':
+    main()
